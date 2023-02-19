528c91f1cf140cac01e7f26d3f4182922cfe08b4
ashala-mahendhar <125733745+ashala-mahendhar@users.noreply.github.com>
2023-02-20 00:04:32 +0530

    Add files via upload

---
+++ JAVASCRIPT/index.js
@@ -0,0 +1,37 @@
+
+function clicksound()
+{
+    var clicksound=new Audio()
+    clicksound.src="./ASSESTS/VIDEOS/fx-cartoon-94472.mp3"
+    clicksound.play()
+    document.getElementById("a").style.backgroundImage="url('./ASSESTS/IMAGES/night-sky-g1219179ba_1920.jpg')"
+}
+function play()
+{
+    var clicksound=new Audio()
+    clicksound.src="./ASSESTS/VIDEOS/3rd mus.mp3"
+    clicksound.play()
+    
+}
+function fun()
+{
+    var a=new Audio()
+    a.src="./ASSESTS/VIDEOS/5th mus.mp3"
+    a.play()
+   
+}
+function fun2()
+{
+    var a=new Audio()
+    a.src="./ASSESTS/VIDEOS/suspend-sound-113941.mp3"
+    a.play()
+    
+}
+function fun3()
+{
+    var a=new Audio()
+    a.src="./ASSESTS/VIDEOS/wow-113128.mp3"
+    a.play()
+    
+    document.getElementById("a").style.backgroundImage="url('./ASSESTS/IMAGES/4th pic.jpg')"
+}
